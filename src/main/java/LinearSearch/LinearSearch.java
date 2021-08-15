446d577bd071dd7abe46afdbe99e9073eaf60e6a
christma <liuchuanmumumu@gmail.com>
2021-08-15 20:32:46 +0800

    LinearSearch

--- src/main/java/LinearSearch/LinearSearch.java
+++ src/main/java/LinearSearch/LinearSearch.java
@@ -16,10 +16,36 @@ public class LinearSearch {
         return -1;
     }
 
+
+    public static <E> int search(E[] data, E target) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals(target)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
     public static void main(String[] args) {
         int[] data = {24, 18, 12, 9, 16, 66, 32, 4};
         int res = LinearSearch.search(data, 16);
+
+
+        Integer[] datas = {24, 18, 12, 9, 16, 66, 32, 4};
+        int search = LinearSearch.search(datas, 666);
+        System.out.println(search);
         System.out.println(res);
+
+
+        Student[] students = {new Student("Alice"),new Student("Bobo"),new Student("Charles")};
+
+        Student bobo = new Student("Bobo");
+
+
+        int search1 = LinearSearch.search(students, bobo);
+        System.out.println(search1);
+
     }
 
 }
